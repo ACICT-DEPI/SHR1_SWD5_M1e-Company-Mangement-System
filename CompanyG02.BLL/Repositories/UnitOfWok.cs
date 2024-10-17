b78a2e702496045013217049ed15085173f5923a
moham <moham@DIVMOASHOR>
2024-10-17 10:33:42 +0300

    up

---
+++ CompanyG02.BLL/Repositories/UnitOfWok.cs
@@ -0,0 +1,33 @@
+﻿using CompanyG02.BLL.Interfaces;
+using CompanyG02.DAL.Contexts;
+using System.Threading.Tasks;
+
+namespace CompanyG02.BLL.Repositories
+{
+    public class UnitOfWok : IUnitOfWok
+    {
+        private readonly CompanyDbContext dbContext;
+
+        public IEmployeeRepository EmployeeRepository { get; set; }
+        public IDepartmentRepository DepartmentRepository { get; set; }
+        public IProjectRepository ProjectRepository { get; set; }
+        public IMyServiceRepository MyServiceRepository { get; set; }
+
+        public UnitOfWok(CompanyDbContext dbContext)
+        {
+            EmployeeRepository = new EmployeeRepository(dbContext);
+            DepartmentRepository = new DepartmentRepository(dbContext);
+            ProjectRepository = new ProjectRepository(dbContext);
+            MyServiceRepository = new MyServiceRepository(dbContext);
+
+            this.dbContext = dbContext;
+        }
+
+        public async Task<int> Compelete()
+       => await dbContext.SaveChangesAsync();
+
+        public void Dispose()
+              => dbContext.Dispose();
+
+    }
+}
